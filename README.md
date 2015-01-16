30237f9e04f60e5e22f9a35fa846ca77fb98c6bc
IHW-Marketing <info@ihw-marketing.eu>
2015-01-16 09:27:11 +0100

    v1.1.2
    * Improve build system
    * Improve CSS
    * Add README

---
+++ README.md
@@ -0,0 +1,1 @@
+# A demo for the jQuery placeholder fallback
--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,41 @@
 var gulp = require('gulp'),
     del = require('del'),
-    runSequence = require('run-sequence'),
     plugins = require('gulp-load-plugins')();
 
 
 // Clean up the `dist` directory
-gulp.task('clean', function() {
+gulp.task('clean', function(callback) {
     del([
         'dist/*',
         '!dist/js',
         'dist/js/*',
         '!dist/js/jquery.min.js'
-    ], { dot: true });
+    ], { dot: true }, callback);
 });
 
 // Copy over the HTML files and update the dependencie names
-gulp.task('copy:html', function() {
+gulp.task('copy:html', ['clean'], function() {
     return gulp.src('src/**/*.html')
-        .pipe(plugins.replace('.css', '.min.css'))
-        .pipe(plugins.replace('.js', '.min.js'))
+        .pipe(plugins.replace(/\.(?=js|css)/g, '.min.'))
         .pipe(gulp.dest('dist'));
 });
 
 // Copy over the CSS files and minify them
-gulp.task('copy:css', function() {
-    return gulp.src('src/css/*.css')
+gulp.task('copy:css', ['clean'], function() {
+    return gulp.src('src/**/*.css')
         .pipe(plugins.minifyCss({ keepSpecialComments: 1 }))
         .pipe(plugins.rename(function(path) {
             path.basename += '.min';
         }))
         .pipe(plugins.replace('*/', '*/\n'))
         .pipe(plugins.insert.append('\n'))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('dist'));
 });
 
 // Copy over the JavaScript files and minify them
-gulp.task('copy:js', function() {
+gulp.task('copy:js', ['clean'], function() {
     return gulp.src([
-            'src/js/*.js',
+            'src/**/*.js',
             '!src/js/jquery.js'
         ])
         .pipe(plugins.jscs())
@@ -48,24 +46,23 @@ gulp.task('copy:js', function() {
             path.basename += '.min';
         }))
         .pipe(plugins.insert.append('\n'))
-        .pipe(gulp.dest('dist/js'));
+        .pipe(gulp.dest('dist'));
 });
 
 // Copy over all other files
-gulp.task('copy:files', function() {
+gulp.task('copy:files', ['copy:html', 'copy:css', 'copy:js'], function() {
     return gulp.src([
             'src/**',
-            '!src/css/*',
-            '!src/js/*'
+            '!src/**/*.css',
+            '!src/**/*.html',
+            '!src/**/*.js'
         ], { dot: true })
         .pipe(plugins.conflict('dist', { skipAll: true }))
         .pipe(gulp.dest('dist'));
 });
 
 // Build task
-gulp.task('build', function() {
-    runSequence('clean', 'copy:html', 'copy:css', 'copy:js', 'copy:files');
-});
+gulp.task('build', ['copy:html', 'copy:css', 'copy:js', 'copy:files']);
 
 // The default task (called when you run `gulp` from CLI)
 gulp.task('default', ['build']);
